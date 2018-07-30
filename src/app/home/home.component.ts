b7d6df1ea755bbb6e64498e80f814013b93a1154
Sifiso Mahlangu <sifiso.mahlangu@reverside.co.za>
2018-07-30 22:45:02 +0200

    commit 30/07

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { SpinnerService } from '../service-spinner/spinner-service';
+import { HttpClient } from '../../../node_modules/@angular/common/http';
+import { ProductWrapper } from '../data-models/product-wrapper-model';
 
 @Component({
   selector: 'app-home',
@@ -7,10 +9,28 @@ import { SpinnerService } from '../service-spinner/spinner-service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit, AfterViewInit {
+  products: ProductWrapper[];
+  showContent: boolean;
 
-  constructor(public sp: SpinnerService) { }
+  constructor(public spinner: SpinnerService, private httpClient: HttpClient) { }
 
   ngOnInit(): void {
+
+    this.spinner.showSpinner();
+    // Fire request to fetch category producs
+    this.httpClient.get<ProductWrapper[]>('/BAKERY/getHomeProducts').subscribe(
+      (response) => {
+
+        this.products = response['homeProducts'];
+        this.showContent = true;
+        this.spinner.hideSpinner();
+      },
+      (error) => {
+        console.log('fetching products error ===>', error);
+        this.showContent = true;
+        this.spinner.hideSpinner();
+      }
+    );
   }
 
   ngAfterViewInit(): void {
