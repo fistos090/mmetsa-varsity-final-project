79701672f7f98043a1cdfc0d18cb62fa6f1fb111
Sifiso Mahlangu <sifiso.mahlangu@reverside.co.za>
2018-08-23 14:50:21 +0200

    abcde

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -13,3 +13,4 @@ export const environment = {
  * because it will have performance impact when throw error
  */
 // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+// git config --global http.proxy http[s]://absmbnw:Mmetsa%40071@proxy-array.absa.co.za:80
